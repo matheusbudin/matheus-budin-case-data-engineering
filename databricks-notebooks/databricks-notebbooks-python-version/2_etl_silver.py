8b9765e8b5d5a496a05920ea706dc6ca6df04d0e
matheusbudin <matheusbudin@hotmail.com>
2024-11-24 17:22:18 -0300

    feat: sorting every archive by type

--- databricks-notebooks/databricks-notebbooks-python-version/2_etl_silver.py
+++ databricks-notebooks/databricks-notebbooks-python-version/2_etl_silver.py
@@ -136,9 +136,9 @@ df_clean = df_clean.withColumn("ts_loadtimestamp_silver", current_timestamp())
 
 # COMMAND ----------
 
-# DBTITLE 1,save it partitioned by ts_load from bronze ingestion
+
 delta_table_path = '/mnt/files/silver/'
-df_clean.write.partitionBy("ts_load_timestamp") \
+df_clean.write.partitionBy("state") \
     .format("delta") \
     .mode("overwrite") \
     .save(delta_table_path)
